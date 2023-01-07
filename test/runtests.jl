ba7c7e997ab4f6482b3abb1765e82b1cca6850a0
anand jain <anandj@uchicago.edu>
2023-01-06 22:50:37 -0800

    fix ci

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,5 +12,7 @@ LIBROSA = pyimport("librosa")
 YOUTUBE_DL = pyimport("youtube_dl")
 
 outdir = joinpath(@__DIR__, "../data/video")
+@test !ispath(outdir)
 video_url = "https://www.youtube.com/watch?v=BaW_jenozKc"
 GLieBLie.doit(video_url, outdir)
+@test !isempty(readdir(outdir))
